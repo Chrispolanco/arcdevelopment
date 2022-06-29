cdff615eb8c55de806d7bb96f1dd77777e3ea5ce
Chrispolanco <chrispolanco@sbcglobal.net>
2022-06-28 22:17:51 -0700

    imported AppBar and ToolBar from Materia UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react'; 
+import Header from './ui/Header';
 
 function App() {
   return (
     <div className="App">
-      Welcome to the world of tomorrow!!
+      <Header/>
     </div>
   );
 }
